ed029247eec3ef55cc96151a396016d0e2c7215b
Aashay <aashayerp@gmail.com>
2022-10-09 05:50:28 +0000

    Exported Fixtures on 09-10-22

--- gspl/hooks.py
+++ gspl/hooks.py
@@ -133,7 +133,7 @@ override_doctype_class = {
 
 
 # TODO: Uncomment and test below fixtures for TASK-17
-fixtures = ["Custom Field"]
+fixtures = ["Custom Field", "Property Setter", "Client Script"]
 # fixtures = [
 # 	{
 # 		"dt": "Custom Field",
