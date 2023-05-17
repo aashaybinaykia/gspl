aee79f573aad6f859e6d7bd9e6f40723132673cf
Harshdeep Mehta <mehta.harsh03@gmail.com>
2023-05-17 08:08:53 +0000

    Validated Item variant for Meters stock UOM

--- gspl/doc_events/item.py
+++ gspl/doc_events/item.py
@@ -7,8 +7,19 @@ from frappe import _
 
 @frappe.whitelist()
 def before_validate(doc, method):
+    validate_item_variant_for_meters_uom(doc)    
     if doc.is_new():
         set_default_item_attributes(doc)
+
+
+def validate_item_variant_for_meters_uom(doc):
+    if doc.stock_uom == 'Meter':
+        if not doc.has_variants and not doc.variant_of:
+            frappe.throw(_("Either has_variants should be true, or variant_of should be set, if stock_uom is 'Meters'"))
+    else:
+        if doc.has_variants:
+            frappe.throw(_("has_variants should be false, if stock_uom is not 'Meters'"))
+
 
 def set_default_item_attributes(doc):
     """
